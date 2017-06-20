72bfae2378c33d71b4c18f675676d586f6d1d18c
circran <alan.m.carando@gmail.com>
2017-06-20 16:10:37 -0300

    TP4

---
+++ TP4/Navegador TP-4 - AlumnoV2/Navegador TP-4 - AlumnoV2/Archivos/Texto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string _archivo;
+            
+        public Texto(string archivo)
+        {
+            this._archivo = Environment.CurrentDirectory + "\\" + archivo;
+        }
+
+
+        /// <summary>
+        /// Escribe en un archivo de texto lo que recibe por el parametro datos
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool guardar(string datos)
+        {
+            bool retorno = false;
+
+            try
+            {
+                using (StreamWriter archivo = new StreamWriter(this._archivo, true))
+                {
+                    archivo.WriteLine(datos);
+                    archivo.Close();
+                }
+
+                retorno = true;
+                
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Lee linea por linea un archivo texto, agregando cada linea en una lista de tipo String.
+        /// </summary>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool leer(out List<string> datos)
+        {
+            datos = new List<string>();
+            bool retorno = false;
+            string aux;
+            
+            try
+            {
+                using (StreamReader archivo = new StreamReader(this._archivo))
+                {
+                    while ((aux = archivo.ReadLine()) != null)
+                    {
+                        datos.Add(aux);
+                    }
+                }
+                retorno = true;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ TP4/Navegador TP-4 - AlumnoV2/Navegador TP-4 - AlumnoV2/Navegador/frmHistorial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class frmHistorial : Form
+    {
+        public const string ARCHIVO_HISTORIAL = "historico.dat";
+
+        public frmHistorial()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Se obtendra una lista con las paginas desde el achivo de texto, esta lista sera mostrada en pantalla 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmHistorial_Load(object sender, EventArgs e)
+        {
+            List<string> paginas = new List<string>();
+            Archivos.Texto archivos = new Archivos.Texto(frmHistorial.ARCHIVO_HISTORIAL);
+
+            archivos.leer(out paginas);
+
+            foreach (string url in paginas)
+            {
+                lstHistorial.Items.Add(url);
+            }
+        }
+    }
+}
